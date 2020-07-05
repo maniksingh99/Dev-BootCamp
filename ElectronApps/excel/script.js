3e2606c53d734adc2138dde32e5b1f4dcbcdadd0
maniksingh99 <maniksingh99@gmail.com>
2020-07-06 04:22:23 +0530

    Changes in excel app

--- ElectronApps/excel/script.js
+++ ElectronApps/excel/script.js
@@ -1,5 +1,6 @@
 const $=require("jquery");
-
+const fs=require("fs");
+const dialog=require("electron").remote.dialog;
 
 $(document).ready(function(){
     let db;
@@ -10,12 +11,75 @@ $(document).ready(function(){
         $('#text-input').val(value);
     })
 
-    //this is fullfilling two cases when value=>value and 
+    $("#New").on("click",function(){
+        db=[];
+        let allRows=$("#grid").find(".row");
+        //console.log(allRows);
+        for(let i=0;i<allRows.length;i++){
+            let row=[];
+            let allCols=$(allRows[i]).find(".cell");
+            for(let j=0;j<allCols.length;j++){
+                let cell={
+                    value:"",
+                    formula:"",
+                    downstream:[],
+                    upstream:[]
+                }
+                $(allCols[j]).html('');
+                row.push(cell);
+            }
+            db.push(row);
+        }
+        console.log(db);
+        $("#grid .cell").eq(0).trigger("click");
+        // let cellArr=$("#grid .cell");
+        // $(cellArr[0]).trigger("click");
+    })
+
+    $("#Save").on("click",async function(){
+        let sbd=await dialog.showOpenDialog();
+        let fp=sbd.filePaths[0];
+        if(fp==undefined){
+            console.log("Please select file first");
+            return;
+        }
+        let jsonData=JSON.stringify(db);
+        fs.writeFileSync(fp,jsonData);
+    })
+
+    $("#Open").on("click",async function(){
+        let sbd=await dialog.showOpenDialog();
+        let fp=sbd.filePaths[0];
+        if(fp==undefined){
+            console.log("Please select file first");
+            return;
+        }
+        let buffer=fs.readFileSync(fp);
+        db=JSON.parse(buffer);
+        let allRows=$("#grid").find(".row");
+        for(let i=0;i<allRows.length;i++){
+            let allCols=$(allRows[i]).find(".cell");
+            for(let j=0;j<allCols.length;j++){
+                $(`#grid .cell[row-id=${i}][col-id=${j}]`).html(db[i][j].value);
+            }
+        }
+    })
+
+    //this is fullfilling two cases when value=>value and formula=> val
     $("#grid .cell").on("blur",function(){
         let {colId,rowId}=getrc(this);
-        db[rowId][colId].value=$(this).text();
+        let cellObject=getCell(this);
+        // db[rowId][colId].value=$(this).text();
         console.log(db);
         lsc=this;
+        if(cellObject.value==$(this).html()){
+            return;
+        }
+        if(cellObject.formula){
+            deleteFormula(cellObject,this);
+        }
+        cellObject.value=$(this).text();
+        updateCell(rowId,colId,cellObject.value);
     })
 
     //the below function is used to convert val=>formula and from formula=>new formula
@@ -27,11 +91,12 @@ $(document).ready(function(){
         let {colId,rowId}=getrc(lsc);
         if(cellObj.formula){
             //should send lsc
-            deleteFormula(cellObj);//it is used to remove upstream and downstream
+            deleteFormula(cellObj,lsc);//it is used to remove upstream and downstream
         }
         cellObj.formula=$(this).val();
         addFormula(lsc,cellObj.formula);//it is used to set upstream and downstream
-        updateCell(rowId,colId,nval,true);
+        let nVal=evaluate(cellObj);
+        updateCell(rowId,colId,nVal,true);
     })
 
 
@@ -75,9 +140,9 @@ $(document).ready(function(){
     }
 
     //It is used to remove upstream and downstream
-    function deleteFormula(cellObject){
+    function deleteFormula(cellObject,cellElem){
         cellObject.formula="";
-        let {rowId,colId}=getCell(cellObject);
+        let {rowId,colId}=getrc(cellElem);
         for(let i=0;i<cellObject.upstream.length;i++){
             let uso=cellObject.upstream[i];
             let fuso=db[uso.rowId][uso.colId];
@@ -87,6 +152,43 @@ $(document).ready(function(){
             fuso.downstream=fArr;
         }
         cellObject.upstream=[];
+    }
+
+    //upstream => go to your upstream => get there values
+        //(A1 + A11 + A2 )=[(,A1,+,A11,+,A1,)]=>[(,10,+,A11,+,10,)]=>( 10 + A11 + 10 )
+    function evaluate(cellObject){
+        let formula=cellObject.formula;
+        console.log(formula);
+        for(let i=0;i<cellObject.upstream.length;i++){
+            let cuso=cellObject.upstream[i];
+            let colAddress=String.fromCharCode(cuso.colId+65);
+            let cellAddress=colAddress+(cuso.rowId+1);
+            let fusoKiVal=db[cuso.rowId][cuso.colId].value;
+            let formulaCompArr=formula.split(" ");
+            formulaCompArr=formulaCompArr.map(function(ele){
+                if(ele==cellAddress){
+                    return fusoKiVal;
+                }else{
+                    return ele;
+                }
+            })
+            formula=formulaCompArr.join(" ");
+        }
+        console.log(formula);
+        return eval(formula);
+    }
+
+    //set yourself to parents downstream set parent to your upstream
+    function updateCell(rowId,colId,nVal){
+        let cellObject=db[rowId][colId];
+        cellObject.value=nVal;
+        $(`#grid .cell[row-id=${rowId}][col-id=${colId}]`).html(nVal);
+        for(let i=0;i<cellObject.downstream.length;i++){
+            let dsocordObj=cellObject.downstream[i];
+            let dso=db[dsocordObj.rowId][dsocordObj.colId];
+            let dsonVal=evaluate(dso);
+            updateCell(dsocordObj.rowId,dsocordObj.colId,dsonVal);
+        }
     }
 
 
@@ -107,24 +209,25 @@ $(document).ready(function(){
 
 
     function init(){
-        db=[];
-        let allRows=$("#grid").find(".row");
-        //console.log(allRows);
-        for(let i=0;i<allRows.length;i++){
-            let row=[];
-            let allCols=$(allRows[i]).find(".cell");
-            for(let j=0;j<allCols.length;j++){
-                let cell={
-                    value:"",
-                    formula:"",
-                    downstream:[],
-                    upstream:[]
-                }
-                row.push(cell);
-            }
-            db.push(row);
-        }
-        console.log(db);
+        $("#New").trigger("click");
+        // db=[];
+        // let allRows=$("#grid").find(".row");
+        // //console.log(allRows);
+        // for(let i=0;i<allRows.length;i++){
+        //     let row=[];
+        //     let allCols=$(allRows[i]).find(".cell");
+        //     for(let j=0;j<allCols.length;j++){
+        //         let cell={
+        //             value:"",
+        //             formula:"",
+        //             downstream:[],
+        //             upstream:[]
+        //         }
+        //         row.push(cell);
+        //     }
+        //     db.push(row);
+        // }
+        // console.log(db);
     }
     init();
 })
