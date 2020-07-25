4d0222fde557a455b3952291fc14420fe4426b72
maniksingh99 <maniksingh99@gmail.com>
2020-07-26 05:04:45 +0530

    added jstree

--- ElectronApps/VS_Code_Clone/index.js
+++ ElectronApps/VS_Code_Clone/index.js
@@ -1,30 +1,73 @@
 const $ = require("jquery");
+const path = require("path");
+const fs = require("fs");
 require("jstree");
-const nodePath = require('path');
 
 $(document).ready(function(){
-
-    let currPath = process.cwd();
-    console.log(currPath);
-    console.log(getName(currPath));
-
-    let data = [];
-    let baseObj={
-        id:currPath,
+    let parentPath = process.cwd();
+    let name = path.basename(parentPath);
+    let data = [{
+        id:parentPath,
         parent:"#",
-        text:getName(currPath)
-    };
-
-    data.push(baseObj);
-
-    $("#file-explorer").jstree({
+        text:name
+    }]
+    let childArr = createData(parentPath);
+    data = [...data,...childArr];
+    console.log(data);
+    $("#tree").jstree({
         "core":{
+            "check_callback":true,
             "data":data
         }
+    }).on("select_node.jstree",
+    function(e,data){
+        // console.log(data);
+        let childNodePath = data.node.id;
+        let childNodeArr = createData(childNodePath);
+        for(let i = 0; i < childNodeArr.length;i++){
+            $('#tree').jstree().create_node(childNodePath,childNodeArr[i],"last");
+        }
     })
-
-    function getName(path){
-        return nodePath.basename(path);
+    function createData(parentPath){
+        let childrens = fs.readdirSync(parentPath);
+        let cdata = [];
+        for(let i = 0; i < childrens.length;i++){
+            let childPath = path.join(parentPath,childrens[i]);
+            let obj={
+                id:childPath,
+                parent:parentPath,
+                text:childrens[i]
+            }
+            cdata.push(obj);
+        }
+        return cdata;
     }
-
 })
+
+
+// $(document).ready(function(){
+
+//     let currPath = process.cwd();
+//     console.log(currPath);
+//     console.log(getName(currPath));
+
+//     let data = [];
+//     let baseObj={
+//         id:currPath,
+//         parent:"#",
+//         text:getName(currPath)
+//     };
+
+//     data.push(baseObj);
+
+//     $("#file-explorer").jstree({
+//         "core":{
+//             "data":data
+//         }
+//     })
+
+//     function getName(path){
+//         return nodePath.basename(path);
+//     }
+
+// })
