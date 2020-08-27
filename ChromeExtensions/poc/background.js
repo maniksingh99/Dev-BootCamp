b09e2e505331543736c25da57c94f3cdf26acd1b
maniksingh99 <maniksingh99@gmail.com>
2020-08-28 03:02:01 +0530

    Made minor changes

--- ChromeExtensions/poc/background.js
+++ ChromeExtensions/poc/background.js
@@ -4,7 +4,7 @@ console.log("Iam in background.js");
 //     console.log(tab);
 // })
 
-chrome.tabs.onUpdated,addListener(function(tabId,changeInfo,tab){
+chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
     if(tab.url.includes("facebook")){
         chrome.tabs.remove(tabId,function(){
             console.log("Tab closed");
